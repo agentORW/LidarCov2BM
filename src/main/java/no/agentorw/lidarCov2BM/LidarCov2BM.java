6e74b6ad2673dc3ab0a8a5b572ba6d976c5fbfe6
agentORW <oskar.wisnes@gmail.com>
2025-10-12 17:14:08 +0200

    Initial commit

---
+++ src/main/java/no/agentorw/lidarCov2BM/LidarCov2BM.java
@@ -0,0 +1,17 @@
+package no.agentorw.lidarCov2BM;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LidarCov2BM extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
