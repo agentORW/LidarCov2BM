d04d6341b883dd58c8d29ba7e6a86bf42d1647af
agentORW <oskar.wisnes@gmail.com>
2025-10-13 18:00:10 +0200

    absolutely useless commit
    i swear im not doing this to get a green square

--- src/main/java/no/agentorw/lidarCov2BM/LidarCov2BM.java
+++ src/main/java/no/agentorw/lidarCov2BM/LidarCov2BM.java
@@ -2,16 +2,20 @@ package no.agentorw.lidarCov2BM;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class LidarCov2BM extends JavaPlugin {
+    public final Logger log =  getLogger();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // Plugin startup logi
+        log.info("LidarCov2BM enabled");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        log.info("LidarCov2BM disabled");
     }
 }
