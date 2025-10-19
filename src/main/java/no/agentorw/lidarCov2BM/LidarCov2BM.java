99fcd2baf4c7b4922a3d035b5e6aab5a0737ef58
agentORW <oskar.wisnes@gmail.com>
2025-10-19 20:02:30 +0200

    Add ShapeMarker to BlueMap integration and update dependencies
    Introduced a ShapeMarker example alongside the existing POIMarker in the BlueMap integration. Updated bluemap-api dependency to version 2.7.6 and specified BlueMap as a required server dependency in paper-plugin.yml.

--- src/main/java/no/agentorw/lidarCov2BM/LidarCov2BM.java
+++ src/main/java/no/agentorw/lidarCov2BM/LidarCov2BM.java
@@ -1,11 +1,17 @@
 package no.agentorw.lidarCov2BM;
 
+import com.flowpowered.math.vector.Vector2d;
 import de.bluecolored.bluemap.api.BlueMapAPI;
 import de.bluecolored.bluemap.api.BlueMapMap;
 import de.bluecolored.bluemap.api.markers.MarkerSet;
 import de.bluecolored.bluemap.api.markers.POIMarker;
+import de.bluecolored.bluemap.api.markers.ShapeMarker;
+import de.bluecolored.bluemap.api.math.Shape;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.Vector;
 import java.util.logging.Logger;
 
 public final class LidarCov2BM extends JavaPlugin {
@@ -25,11 +31,20 @@ public final class LidarCov2BM extends JavaPlugin {
     }
 
     private void onBlueMapEnable(BlueMapAPI api) {
+        log.info("LidarCov2BM onBlueMapEnable");
         POIMarker marker = POIMarker.builder()
                 .label("My Marker")
-                .position(20.0, 65.0, -23.0)
-                .minDistance(100)
+                .position(3494494.0, 65.0, -6387419)
                 .maxDistance(1000)
+                .build();
+
+        Shape rectangle = Shape.builder()
+                .addPoints(new Vector2d(10.0, 10.0), new Vector2d(-10.0, 10.0), new Vector2d(10.0, 20.0))
+                .build();
+
+        ShapeMarker shapeMarker = ShapeMarker.builder()
+                .label("Whats up")
+                .shape(rectangle, 50)
                 .build();
 
         MarkerSet markerSet = MarkerSet.builder()
@@ -38,6 +53,9 @@ public final class LidarCov2BM extends JavaPlugin {
 
         markerSet.getMarkers()
                 .put("my-marker-id", marker);
+
+        markerSet.getMarkers()
+                .put("my-shape-id", shapeMarker);
 
         api.getWorld("world").ifPresent(world -> {
             for (BlueMapMap map : world.getMaps()) {
